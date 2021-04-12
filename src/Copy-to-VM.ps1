d7a90b0bd2681f0bb594d0afd43fb8356af8bf1e
Michael A. Agarenzo <magarenzo@gmail.com>
2021-04-12 17:04:32 -0400

    Improve comments and logging.

--- src/Copy-to-VM.ps1
+++ src/Copy-to-VM.ps1
@@ -1,14 +1,59 @@
-<# Copy files from host Windows machine to local virtual machine #>
+<#
+.SYNOPSIS
+    Copy files from host Windows machine to local virtual machine
 
-param (
+.DESCRIPTION
+    Found this nifty PowerShell cmdlet "Copy-VMFile" to copy a file to a virtual machine, so I put it inside this script with parameters and simple logging for ease of use.
+
+.PARAMETER VirtualMachineName
+    Name of the virtual machine we are copying the file to.
+
+.PARAMETER PathSource
+    Full path to the source file we are copying from the host machine, including the file name.
+
+.PARAMETER PathDestination
+    Full path of the destination we are copying to on the VM, including the new file name.
+
+.EXAMPLE
+    PS >.\Copy-To-VM.ps1 `
+    -VirtualMachineName "Example-VM-Name" `
+    -SourcePath "C:\Example-File.txt" `
+    -DestinationPath "D:\Example-Directory\Example-File.txt" `
+#>
+
+Param (
     $VirtualMachineName="",
     $SourcePath="",
     $DestinationPath=""
 )
 
-Write-Output "$(Get-Date -Format 'HH:mm:ss') - STARTED - Copy to VM"
+Function WriteLog {
+    Param (
+        [string] $Message,
+        [switch] $Err
+    )
+    $LogFile = "Copy-to-VM.log"
+    If (!(Test-Path $LogFile)) {
+        New-Item $LogFile
+    }
+    $Now = Get-Date -Format "HH:mm:ss"
+    $Line = "$Now`t$Message"
+    $Line | Add-Content $LogFile -Encoding UTF8
+    If ($Err) {
+        Write-Host $Line -ForegroundColor Red
+    } Else {
+        Write-Host $Line
+    }
+}
 
-$Command = "Copy-VMFile $VirtualMachineName -SourcePath $SourcePath -DestinationPath $DestinationPath -CreateFullPath -FileSource Host"
-Invoke-Expression $Command
+WriteLog "STARTED: Copy-to-VM.ps1."
 
-Write-Output "$(Get-Date -Format 'HH:mm:ss') - COMPLETED - Copy to VM"
+Try {
+    $Command = "Copy-VMFile $VirtualMachineName -SourcePath $SourcePath -DestinationPath $DestinationPath -CreateFullPath -FileSource Host"
+    Invoke-Expression $Command
+} Catch {
+    WriteLog -Err "ERROR: Copying file to VM."
+}
+
+WriteLog "COMPLETED: Copy-to-VM.ps1."
+WriteLog "--------------------------------------------------"
